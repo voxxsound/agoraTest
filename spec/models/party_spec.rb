950af0d71a1c0ded3359cd4959c5b01bd1d644d4
Aaron Barker <voxxsound@gmail.com>
2010-11-24 10:32:46 -0500

    party spec creation

--- spec/models/party_spec.rb
+++ spec/models/party_spec.rb
@@ -3,14 +3,24 @@ require 'spec_helper'
 describe Party do
   before(:each) do
     @valid_attributes = {
-      :first_name => "value for first_name",
-      :last_name => "value for last_name",
-      :address => "value for address",
-      :phone => "value for phone",
-      :email => "value for email"
+      :first_name => "John",
+      :last_name => "Daniels",
+      :address => "1 Some St.",
+      :city  => "Some City",
+      :state => "NY",
+      :zip => "11223"
+      :phone => "(123) 456-7890",
+      :email => "dhh@37signals.com"
     }
   end
-
+    
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:city) }
+  it {should validate_presence_of(:zip) }
+    
+  end
   it "should create a new instance given valid attributes" do
     Party.create!(@valid_attributes)
   end
